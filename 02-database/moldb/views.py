7d5f3567773ddef0a6a00405c6d06c0fc7518088
gorgitko <fg-42@seznam.cz>
2016-02-26 16:31:50 +0100

    simplified; added api_uploadMolecules()

--- 02-database/moldb/views.py
+++ 02-database/moldb/views.py
@@ -5,8 +5,10 @@ from django.template import RequestContext
 from .forms import AddMoleculeForm
 from django.http import JsonResponse
 from rdkit import Chem
-from .helper_functions import addMoleculeJsonResponse
+from .helper_functions import addMoleculeDictSerializer
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+import json
+from django.utils.datastructures import MultiValueDictKeyError
 
 def home(request):
     return render_to_response('home.html',
@@ -46,7 +48,7 @@ def api_molConverter(request):
         elif format_from == "smiles":
             mol = Chem.MolFromSmiles(data)
         else:
-            return addMoleculeJsonResponse(False, error="input format unknown")
+            return JsonResponse(addMoleculeDictSerializer(False, error="Input format unknown."))
 
         if mol:
             if format_to == "molfile":
@@ -54,27 +56,50 @@ def api_molConverter(request):
             elif format_to == "smiles":
                 output = Chem.MolToSmiles(mol)
             else:
-                return addMoleculeJsonResponse(False, error="Input format unknown.")
+                return JsonResponse(addMoleculeDictSerializer(False, error="Output format unknown."))
 
             if output:
-                return addMoleculeJsonResponse(True, data=output)
+                return JsonResponse({"success": True, "data": output})
         else:
-            return addMoleculeJsonResponse(False, error="Cannot convert from '{}' to '{}': probably invalid data supplied.".format(format_from, format_to))
+            return JsonResponse(
+                addMoleculeDictSerializer(False,
+                                          error="Cannot convert from '{}' to '{}': probably invalid data supplied."
+                                            .format(format_from, format_to)))
 
 def api_addMolecule(request):
     if request.POST and request.POST["molfile"]:
-        molfile = request.POST["molfile"]
+        return JsonResponse([saveMol(molfile=request.POST["molfile"])], safe=False)
 
-        if molfile != "":
-            mol = models.Molecule()
+def api_uploadMolecules(request):
+    try:
+        file = request.FILES["file"]
+    except MultiValueDictKeyError:
+        return JsonResponse([addMoleculeDictSerializer(False, error="No file supplied.", smiles="-")], safe=False)
 
-            try:
-                mol.save(molfile=request.POST["molfile"])
-            except models.Molecule.MoleculeExistsInDatabase:
-                return addMoleculeJsonResponse(False, error="Cannot add the molecule: it already exists in database.")
-            except models.Molecule.MoleculeCreationError:
-                return addMoleculeJsonResponse(False, error="Cannot add the molecule: check your structure (valence etc.).")
+    response = []
 
-            return addMoleculeJsonResponse(True, data=mol.internal_id, smiles=mol.smiles)
-        else:
-            return addMoleculeJsonResponse(False, error="Cannot add empty molecule.")
+    for chunk in file.chunks():
+        for line in str(chunk, encoding="utf-8").split("\n"):
+            response.append(saveMol(smiles=line))
+
+    return JsonResponse(response, safe=False)
+
+# helper functions
+
+def saveMol(smiles=None, molfile=None):
+    if smiles or molfile:
+        mol = models.Molecule()
+
+        try:
+            mol.save(smiles=smiles, molfile=molfile)
+        except models.Molecule.MoleculeExistsInDatabase as e:
+            return addMoleculeDictSerializer(False, error="Cannot add the molecule: it already exists in database.", smiles=e.smiles)
+        except models.Molecule.MoleculeCreationError:
+            if smiles:
+                return addMoleculeDictSerializer(False, error="Cannot add the molecule: check your structure (valence etc.).", smiles=smiles)
+            else:
+                return addMoleculeDictSerializer(False, error="Cannot add the molecule: check your structure (valence etc.).")
+
+        return addMoleculeDictSerializer(True, internal_id=mol.internal_id, smiles=mol.smiles)
+    else:
+        return addMoleculeDictSerializer(False, error="Cannot add empty molecule.")
