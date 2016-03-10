8c1c1430503f177754bfec9fa712fd863979894d
gorgitko <fg-42@seznam.cz>
2016-03-10 12:03:04 +0100

    fixed downloading of molecules

--- 02-database/molinf/urls.py
+++ 02-database/molinf/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
     url(r'^api/addMolecule$', moldb.views.api_addMolecule, name='api_addMolecule'),
     url(r'^api/uploadMolecules$', moldb.views.api_uploadMolecules, name='api_uploadMolecules'),
     url(r'^api/uploadMolecules/status$', moldb.views.api_uploadMoleculesStatus, name='api_uploadMoleculesStatus'),
-    url(r'^api/downloadMolecules$', moldb.views.api_downloadMolecules, name='api_downloadMolecules$')
+    url(r'^api/downloadMolecules$', moldb.views.api_downloadMolecules, name='api_downloadMolecules')
 ]
